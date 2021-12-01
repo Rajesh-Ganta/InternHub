2ba12c50ba53a678955ff96479d2f1989cd9aa3e
Rajesh-Ganta <rajeshganta123456@gmail.com>
2021-12-01 20:12:17 +0530

    apply for Job Notice

--- src/app/shared/application-preview/application-preview.component.ts
+++ src/app/shared/application-preview/application-preview.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AllPurposeService } from 'src/app/allpurposervice.service';
 
@@ -9,11 +10,36 @@ import { AllPurposeService } from 'src/app/allpurposervice.service';
 export class ApplicationPreviewComponent implements OnInit {
 
   data :any;
-  constructor(private allpurpose: AllPurposeService) { }
+  constructor(private allpurpose: AllPurposeService, private http: HttpClient) { }
+
+  user_data : any;
 
   ngOnInit(): void {
     this.data = this.allpurpose.post;
     console.log(this.data);
+    let x:any = JSON.parse(localStorage.getItem('loginData')??'');
+    console.log(x);
   }
+
+  post = this.allpurpose.post;
+
+  eligible: any;
+
+  apply()
+  {
+    if(this.eligible == true)
+    {
+      this.http.post("http://localhost:8000/insert_application",{"student_id": "S160215", "name": 'XXX', "email" : "12345", "phone": "123434", "company_name": "xxx", "notice_id": "11"}).subscribe((res)=>{
+        console.log(res);
+      },(err)=>{
+        console.log(err);
+      },()=>{
+        alert("Application Submited");
+      })
+
+    }
+  }
+
+  //data["student_id"],data["name"],data["email"], data["phone"], data["company_name"], datetime.datetime.now() ,data["notice_id"])
 
 }
